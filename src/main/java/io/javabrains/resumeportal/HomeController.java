373c8b6eb93545de6162cbd8fc2dbdbe62409202
Marc D. Holman <marcholman9323@gmail.com>
2021-07-10 17:51:51 -0500

    initial commit

---
+++ src/main/java/io/javabrains/resumeportal/HomeController.java
@@ -0,0 +1,15 @@
+package io.javabrains.resumeportal;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
+}
